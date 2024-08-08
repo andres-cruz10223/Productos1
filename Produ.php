3dacf95d61129f5cabae0cd854da2287fc6eef51
andres-cruz10223 <andrescruz221996@gmail.com>
2024-08-08 15:28:02 -0500

    Update Produ.php

--- Produ.php
+++ Produ.php
@@ -3,7 +3,7 @@ $No_producto1=$_POST['Producto1']:
   $No_producto2 =$_POST['Producto2];
    $No_producto3 =$_POST['Producto3];
   $Precio=$_POST['Cargo'];
-$suma=suma+$No_producto1+$No_producto2+$No_producto3;
+$suma =suma+$No_producto1+$No_producto2+$No_producto3;
   echo "El nombre el primero producto es : ".$No_producto1;
     echo "El nombre el segundo producto es : ".$No_producto2;
   echo "El nombre el tercer producto es : ".$No_producto3;
